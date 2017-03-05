3a824735fe7684ae8427dca7ce6eb0f916b91ee4
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2017-03-05 17:23:25 -0500

    started to style forms

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
 
-// <><><><><><><><>NAV POP OUT<><><><><><><><><><><><><>
-	$(".menu").on("click", function() {
-		$("nav").toggleClass("overlay");
-	});
-//END NAV POP OUT=======================================
+//Nav popout============================================
+$(".menu").on("click", function() {
+	$("nav").toggleClass("overlay");
+});
 
-// <><><><><><><><><>menu ANIMATION<><><><><><><><><>
-	$(".menu").on("click", function() {
-		$(".top").toggleClass("clicked");
-	});
 
-	$(".menu").on("click", function() {
-		$(".middle").toggleClass("clicked");
-	});
+//Menu animation========================================
+$(".menu").on("click", function() {
+	$(".top").toggleClass("clicked");
+});
 
-	$(".menu").on("click", function() {
-		$(".bottom").toggleClass("clicked");
-	});
-// END menu==========================================
+$(".menu").on("click", function() {
+	$(".middle").toggleClass("clicked");
+});
 
+$(".menu").on("click", function() {
+	$(".bottom").toggleClass("clicked");
+});
+
+//Menu back ground color===============================
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+
+    if (scroll > 4){
+    	$(".menu-container").css("background-color","rgba(24,38,80,0.5)");
+    }else if(scroll < 4){
+    	$(".menu-container").css("background-color","inherit");
+    }
+});
